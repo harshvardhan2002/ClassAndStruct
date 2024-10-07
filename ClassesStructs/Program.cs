c37cb0db153b447adbcf7e610c60262198b75506
Harshvardhan Gupta <76556011+harshvardhan2002@users.noreply.github.com>
2024-10-07 21:21:00 +0530

    Update Program.cs

--- ClassesStructs/Program.cs
+++ ClassesStructs/Program.cs
@@ -13,9 +13,9 @@
             Console.WriteLine("actual name in case of class: "+seperateClass.Name);
             Console.WriteLine("actual name in case of structs: "+seperateStruct.Name);
 
-            StructAndClass.ChangeNameStruct(ref seperateStruct);
+            StructAndClass.ChangeNameStruct(ref seperateStruct); // passed it by reference
             SeperateClass.ChangeNameClass(seperateClass);
-
+//The call to SeperateClass.ChangeNameClass(seperateClass) also changes the name in the class to "Changed Class" without needing ref because it’s a reference type.
             Console.WriteLine("current name in case of class: " + seperateClass.Name);
             Console.WriteLine("current name in case of structs: " + seperateStruct.Name);
         }
